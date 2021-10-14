b406a0fc080887f1dbdbd8898f450dbddc084642
aleks <aleksander.skierka@gmail.com>
2021-10-14 22:35:45 +0200

    1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
-
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
-
+        System.out.println("Witaj świecie!");
+        System.out.print("1234");
+        System.out.printf("as%ndf");
 
 
     }
