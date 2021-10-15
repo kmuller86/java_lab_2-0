8704a76e872f190a4868252eba47b523816b32ca
aleks <aleksander.skierka@gmail.com>
2021-10-15 20:02:24 +0200

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -16,6 +14,45 @@ public class Main {
         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
         *  */
+
+        int a = 7;
+        double b = 19;
+        byte c = 127;
+        short d = 32767;
+        long e = 666;
+        float f = 17.37f;
+        double g = 36.458;
+        boolean h = true;
+        char i = 'z';
+        String q = "Polska";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(q);
+
+        int A = 100;
+        int B = 5;
+
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+
+        double X = 666;
+        double Y = 15;
+
+        System.out.println(X + Y);
+        System.out.println(X - Y);
+        System.out.println(X * Y);
+        System.out.println(X / Y);
+        System.out.println(X % Y);
 
 
 
