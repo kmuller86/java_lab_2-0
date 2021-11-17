749de2259ef26991f33a294cffe442ffca1ebe68
Aleksander <aleksander.skierka@gmail.com>
2021-11-17 18:57:26 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,21 @@ public class Main {
         *   dane mają być wprowadzane z klawiatury w konsoli
         *   b) wyświetlić dane za pomocą println i printf
         */
+        Scanner scan = new Scanner(System.in);
 
+        System.out.print("Podaj imię: ");
+        String imie = scan.next();
 
+        System.out.print("Podaj nazwisko: ");
+        String nazwisko = scan.next();
+
+        System.out.print("Podaj wiek: ");
+        int wiek = scan.nextInt();
+
+        System.out.print("Podaj numer albumu: ");
+        int album = scan.nextInt();
+
+        System.out.println("Nazywasz się " + imie + " " + nazwisko + ", masz " + wiek + " lat i numer albumu: " + album+"");
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
         */
