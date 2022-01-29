adede7da9cd9a8accf0b57ce749e704e86e2f84e
kmuller86 <89707079+kmuller86@users.noreply.github.com>
2022-01-29 11:09:43 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
+        /* miała być tablica double[] */
         int [] tab1 = {1, 2, 3, 6, 5};
 
         for (int i = 0; i < tab1.length; i++) {
